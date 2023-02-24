d443a7176d52cdcbdddb28225c8c08d22bad1c51
Justin Verthein <Justinmackay88@hotmail.com>
2023-02-23 16:03:44 -0800

    defined my constants and variables in my index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const apiUrl = 'https://www.boredapi.com/api/activity';
+
+// DOM elements 
+const fetchActivityBtn = document.getElementById('fetch-activity-btn');
+const activity1 = document.getElementById('activity');
+const activityList1 = document.getElementById('activity-list');
