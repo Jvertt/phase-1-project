a896eacfc8239ea8691f2e83414c8b2d99223c8d
Justin Verthein <Justinmackay88@hotmail.com>
2023-02-23 16:34:35 -0800

    created a function that fetches data from the api, then I created an event listener for this function so that every time the activity button is clicked a random activity is displayed

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const apiUrl = 'https://www.boredapi.com/api/activity';
 const fetchActivityBtn = document.getElementById('fetch-activity-btn');
 const activity1 = document.getElementById('activity');
 const activityList1 = document.getElementById('activity-list');
+
+fetchActivityBtn.addEventListener('click', fetchActivity);
+
+function fetchActivity(){
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => activity1.innerText = data.activity)
+.catch(error => console.error(error));
+}
