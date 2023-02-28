03bcc193d94bdfdec08ee4147953ce34398d912c
Justin Verthein <Justinmackay88@hotmail.com>
2023-02-28 15:23:01 -0800

    created a helper function to fetch data from the api

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const apiUrl = 'https://rickandmortyapi.com/api/character';
+const apiUrl = 'https://rickandmortyapi.com/api/character'
+
+function fetchdata(url){
+    return fetch(url)
+    .then(response => {
+        if(!response.ok){
+            throw new Error(`Network error: ${response.status}`);
+        }
+        return response.json();
+    })
+}
