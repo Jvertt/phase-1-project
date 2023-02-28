94ba712e2fcc53647f6d19a76fd7b646d7498ea8
Justin Verthein <Justinmackay88@hotmail.com>
2023-02-28 11:52:15 -0800

    make some custom buttons using css

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const apiUrl = 'https://www.boredapi.com/api/activity';
 const fetchActivityBtn = document.getElementById('fetch-activity-btn');
 const activity1 = document.getElementById('activity');
 const activityList1 = document.getElementById('activity-list');
+const toggleModeBtn = document.getElementById('toggleModeBtn');
+const body1 = document.querySelector('body')
 
 // Event listeners 
 fetchActivityBtn.addEventListener('click', fetchActivity);
+toggleModeBtn.addEventListener('click', () => {
+    body1.classList.toggle('dark');
+    body1.classList.toggle('light');
+}); 
 
 
 
@@ -17,12 +23,3 @@ fetch(apiUrl)
 .then(data => activity1.innerText = data.activity)
 .catch(error => console.error(error));
 }
-
-
-const toggleModeBtn = document.getElementById('toggleModeBtn');
-const body1 = document.querySelector('body')
-
-toggleModeBtn.addEventListener('click', () => {
-    body1.classList.toggle('dark');
-    body1.classList.toggle('light');
-}); 
