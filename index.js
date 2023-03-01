aa4c1bfbfbac6d4526ddbe2f1d3a01900342266e
Justin Verthein <Justinmackay88@hotmail.com>
2023-02-28 16:25:40 -0800

    created a function that renders a list of characters as html elements with a specific class and content

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const apiUrl = 'https://rickandmortyapi.com/api/character'
 
-function fetchdata(url){
+function fetchData(url){
     return fetch(url)
     .then(response => {
         if(!response.ok){
             throw new Error(`Network error: ${response.status}`);
         }
         return response.json();
+    })
+}
+
+// event listener for search button
+
+document.getElementById('searchButton').addEventListener("click", () => {
+    const searchTerm = document.getElementById('searchInput').value;
+    fetchData(`${apiUrl}?name=${searchTerm}`)
+    .then(characters => renderCharacters(characters.results))
+    .catch(error => console.error(error));
+})
+
+function renderCharacters(characters) {
+    const charactersList = document.getElementById('charactersList')
+    charactersList.innerHtml = '';
+
+    characters.forEach((character) => {
+        const card = document.createElement('div');
+        card.className = 'character-card';
+        card.innerHTML = `
+        <img src="${character.image}" alt="${character.name}">
+        <div class ="name">${character.name}</div>
+        <div class="species">${character.species}</div>
+        <div class="status">${character.status}</div>
+        `;
+        charactersList.appendChild(card);
     })
 }
